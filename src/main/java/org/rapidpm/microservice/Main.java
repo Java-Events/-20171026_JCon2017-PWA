39dd44705fccc0238d4804c95af5c5883aeb8dda
Sven Ruppert <sven.ruppert@gmail.com>
2018-02-10 06:28:08 +0100

    final PWA 100percent

--- src/main/java/org/rapidpm/microservice/Main.java
+++ src/main/java/org/rapidpm/microservice/Main.java
@@ -4,18 +4,27 @@ package org.rapidpm.microservice;
 import io.undertow.Handlers;
 import io.undertow.Undertow;
 import io.undertow.UndertowOptions;
+import io.undertow.connector.ByteBufferPool;
 import io.undertow.server.HttpHandler;
+import io.undertow.server.XnioByteBufferPool;
 import io.undertow.server.handlers.PathHandler;
 import io.undertow.servlet.Servlets;
 import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.servlet.api.DeploymentManager;
+import io.undertow.websockets.core.WebSockets;
+import io.undertow.websockets.jsr.WebSocketDeploymentInfo;
 import org.rapidpm.vaadin.helloworld.server.MyProjectServlet;
 import org.rapidpm.vaadin.helloworld.server.PWAServlet;
+import org.xnio.BufferAllocator;
+import org.xnio.ByteBufferSlicePool;
+import org.xnio.Pool;
 
 import javax.servlet.ServletException;
 import java.util.Optional;
 
 import static io.undertow.Handlers.redirect;
+import static io.undertow.UndertowOptions.ENABLE_HTTP2;
+import static io.undertow.UndertowOptions.HTTP2_SETTINGS_ENABLE_PUSH;
 import static io.undertow.servlet.Servlets.servlet;
 
 /**
@@ -37,29 +46,37 @@ public class Main {
 
   public static void main(String[] args) {
 
-    // grap it here
-    // http://bit.ly/undertow-servlet-deploy
+    /*
+     * Create the App WebSocketDeploymentInfo and configure
+     */
+    WebSocketDeploymentInfo appWebSocketDeploymentInfo = new WebSocketDeploymentInfo();
+
+    appWebSocketDeploymentInfo.setBuffers(
+        new XnioByteBufferPool(
+            new ByteBufferSlicePool(BufferAllocator.BYTE_BUFFER_ALLOCATOR,
+                                    17000, 17000 * 16)));
+
     DeploymentInfo servletBuilder
         = Servlets.deployment()
                   .setClassLoader(Main.class.getClassLoader())
                   .setContextPath(CONTEXT_PATH)
                   .setDeploymentName("ROOT.war")
                   .setDefaultEncoding("UTF-8")
+                  .setSecurityDisabled(true)
+                  .addServletContextAttribute(WebSocketDeploymentInfo.ATTRIBUTE_NAME, appWebSocketDeploymentInfo)
                   .addServlets(
                       servlet(
                           PWAServlet.class.getSimpleName(),
                           PWAServlet.class
                       ).addMappings("/sw.js", "/manifest.json", "/VAADIN/app.js")
-
                        .setAsyncSupported(true)
-                       .setEnabled(true),
+                       .setEnabled(false),
                       servlet(
                           MyProjectServlet.class.getSimpleName(),
                           MyProjectServlet.class
                       ).addMapping("/*")
                        .setAsyncSupported(true)
-                       .setEnabled(true)
-
+                       .setEnabled(false)
                   );
 
     DeploymentManager manager = Servlets
@@ -70,12 +87,14 @@ public class Main {
 
     try {
       HttpHandler httpHandler = manager.start();
+
       PathHandler path = Handlers.path(redirect(CONTEXT_PATH))
                                  .addPrefixPath(CONTEXT_PATH, httpHandler);
 
       Undertow undertowServer = Undertow.builder()
                                         .addHttpListener(8080, "0.0.0.0")
-                                        .setServerOption(UndertowOptions.ENABLE_HTTP2, true)
+                                        .setServerOption(ENABLE_HTTP2, true)
+                                        .setServerOption(HTTP2_SETTINGS_ENABLE_PUSH, true)
                                         .setHandler(path)
                                         .build();
       undertowServer.start();
--- src/main/java/org/rapidpm/vaadin/helloworld/server/MyUI.java
+++ src/main/java/org/rapidpm/vaadin/helloworld/server/MyUI.java
@@ -1,6 +1,7 @@
 package org.rapidpm.vaadin.helloworld.server;
 
 import com.vaadin.annotations.Push;
+import com.vaadin.shared.communication.PushMode;
 import org.vaadin.leif.headertags.Link;
 import org.vaadin.leif.headertags.Meta;
 import org.vaadin.leif.headertags.MetaTags;
@@ -19,6 +20,7 @@ import com.vaadin.ui.UI;
 @MetaTags({
     @Meta(name = "viewport", content = "width=device-width, initial-scale=1") ,
     @Meta(name = "theme-color", content = "#404549") ,
+    @Meta(name = "description", content = "some content") ,
 })
 @Title("Vaadin PWA Jumpstart")
 @Push

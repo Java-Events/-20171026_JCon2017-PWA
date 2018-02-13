f595ed95dc4811ce2b9746b1853d70aec933ab18
Sven Ruppert <sven.ruppert@gmail.com>
2018-02-13 10:06:31 +0100

    removed useless link

--- src/main/java/org/rapidpm/vaadin/helloworld/server/MyUI.java
+++ src/main/java/org/rapidpm/vaadin/helloworld/server/MyUI.java
@@ -1,27 +1,25 @@
 package org.rapidpm.vaadin.helloworld.server;
 
-import com.vaadin.annotations.Push;
-import com.vaadin.shared.communication.PushMode;
-import org.vaadin.leif.headertags.Link;
-import org.vaadin.leif.headertags.Meta;
-import org.vaadin.leif.headertags.MetaTags;
 import com.vaadin.annotations.JavaScript;
+import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
+import org.vaadin.leif.headertags.Link;
+import org.vaadin.leif.headertags.Meta;
+import org.vaadin.leif.headertags.MetaTags;
 
 /**
  *
  */
 @JavaScript("vaadin://app.js")
 @Link(rel = "manifest", href = "/manifest.json")
-@Link(rel = "sw.js", href = "/sw.js")
 @MetaTags({
-    @Meta(name = "viewport", content = "width=device-width, initial-scale=1") ,
-    @Meta(name = "theme-color", content = "#404549") ,
-    @Meta(name = "description", content = "some content") ,
-})
+              @Meta(name = "viewport", content = "width=device-width, initial-scale=1"),
+              @Meta(name = "theme-color", content = "#404549"),
+              @Meta(name = "description", content = "some content"),
+          })
 @Title("Vaadin PWA Jumpstart")
 @Push
 public class MyUI extends UI {
